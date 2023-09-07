eb431cd755425902ac479139dfe48074962d292b
hoonih <hoonih1004@gmail.com>
2023-09-08 01:14:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/snackaudiobook/ux/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.snackaudiobook.ux
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.snackaudiobook.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
